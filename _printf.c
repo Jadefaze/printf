03b6e72cc62896690dad474d475b9a6271d1c48b
Jadefaze <mabnoz@gmail.com>
2023-02-22 08:58:21 -0800

    overhaul changes

--- _printf.c
+++ _printf.c
@@ -1,65 +1,35 @@
-#include <stdarg.h>
-#include <unistd.h>
 #include "main.h"
-
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-
-int _putchar(char c)
-{
-	        return (write(1, &c, 1));
-}
-/**
- * _printf - to print anything passed;
- * @format: character string
- * Return: number of characters printed -'\0'
+ * _printf - Receives the main string and all the necessary parameters to
+ * print a formated string
+ * @format: A string containing all the desired characters
+ * Return: A total count of the characters printed
  */
 int _printf(const char *format, ...)
 {
-	va_list ap; /*points to each unnamed arg in turn*/
-	const char *p;
-	char *sval, cval;
-	signed int ival;
+	int printed_chars;
+	conver_t f_list[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{"d", print_integer},
+		{"i", print_integer},
+		{"b", print_binary},
+		{"r", print_reversed},
+		{"R", rot13},
+		{"u", unsigned_integer},
+		{"o", print_octal},
+		{"x", print_hex},
+		{"X", print_heX},
+		{NULL, NULL}
+	};
+	va_list arg_list;
 
-	va_start(ap, format); /*make ap point to 1st un-named arg*/
-	for (p = format; *p; p++)
-	{
-		if (*p != '%' && *p != '\0')
-		{
-			_putchar(*p);
-			continue;
-		}
-		switch (*++p)
-		{
-			case 'c':
-				cval = va_arg(ap, int);
-				_putchar(cval);
-				break;
-			case 's':
-				for (sval = va_arg(ap, char *); *sval; sval++)
-					_putchar(*sval);
-				break;
-			case '%':
-				_putchar('%');
-				break;
-			case 'd':
-				ival = va_arg(ap, int);
-				_putchar(ival);
-				break;
-			case 'i':
-				ival = va_arg(ap, signed int);
-				_putchar(ival);
-				break;
-			default:
-				_putchar('%');
-				_putchar(*p);
-				break;
-		}
-	}
-	va_end(ap); /* clean up when done*/
-	return (0);
+	if (format == NULL)
+		return (-1);
+	va_start(arg_list, format);
+	/*Calling parser function*/
+	printed_chars = parser(format, f_list, arg_list);
+	va_end(arg_list);
+	return (printed_chars);
 }
